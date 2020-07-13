71e4fc0574626df65649f521f93bb9bc907a070a
Alvin Anugrah Arrafi <62147732+anugrah1922@users.noreply.github.com>
2020-07-13 16:59:28 +0700

    Add files via upload

---
+++ data_app (1).sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 13 Jul 2020 pada 11.58
+-- Versi server: 10.1.37-MariaDB
+-- Versi PHP: 7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `app`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_app`
+--
+
+CREATE TABLE `data_app` (
+  `nim` varchar(8) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `jurusan` varchar(50) DEFAULT NULL,
+  `alamat` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
